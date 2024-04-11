b8208295ca2e40ee69ecfe45ee6423cd6a358bd1
allenLQVE <allenlqve@gmail.com>
2024-04-11 11:46:11 -0400

    Be able to connect to only selected room

--- src/application/TestMessenger.java
+++ src/application/TestMessenger.java
@@ -13,23 +13,30 @@ public class TestMessenger {
     private ArrayList<Integer> send_ports = new ArrayList<Integer>(Arrays.asList(5554,5553));
     private int listen_port = 5555;
     private int send_port = 5554;
+    private int currRoom;
 
     public static void main(String[] args) {
+        // connect to mutiple rooms at once
+        // ArrayList<MessageManager> managers = new ArrayList<MessageManager>();
         // MessageManager manager = new SocketMessageManager("localhost");
+        // managers.add(manager);
+        // manager = new SocketMessageManager("localhost");
+        // managers.add(manager);
 
-        ArrayList<MessageManager> managers = new ArrayList<MessageManager>();
+        // TestMessenger testMessenger = new TestMessenger();
+        // testMessenger.start(managers);
+
+        // connect to one room at a time
         MessageManager manager = new SocketMessageManager("localhost");
-        managers.add(manager);
-        manager = new SocketMessageManager("localhost");
-        managers.add(manager);
 
         TestMessenger testMessenger = new TestMessenger();
-        // testMessenger.start(manager);
-        testMessenger.start(managers);
+        testMessenger.start(manager);
     }
 
     /**
-     * Create a process to handle actions from user
+     * Create a process to handle actions from user.
+     * Connect to all the rooms at once.
+     * Have mutiple connections to the server.
      * 
      * @param ArrayList<MessageManager> a list of managers that handle message and connect/disconnect
      */
@@ -41,14 +48,15 @@ public class TestMessenger {
         while(isConnected){
             System.out.println("Please input text:");
             String input = keyboard.nextLine();
+            int room;
 
             switch (input) {
                 case "connect":
+                    System.out.println("Connect to room");
+                    room = keyboard.nextInt();
+                    keyboard.nextLine();
+                    managers.get(room).connect(listener, listen_ports.get(room));
                     
-                    for (int i = 0; i < managers.size(); i++) {
-                        managers.get(i).connect(listener, listen_ports.get(i));
-                    }
-
                     System.out.println("Connected to server");
                     System.out.println("Please input user name");
                     userName = keyboard.nextLine();
@@ -60,7 +68,8 @@ public class TestMessenger {
                     isConnected = false;
                 default:
                     System.out.println("Sending message to room");
-                    int room = keyboard.nextInt();
+                    room = keyboard.nextInt();
+                    keyboard.nextLine();
                     managers.get(room).sendMessage(userName, input, send_ports.get(room), listen_ports.get(room));
             }
         }
@@ -69,7 +78,9 @@ public class TestMessenger {
     }
 
     /**
-     * Create a process to handle actions from user
+     * Create a process to handle actions from user.
+     * Connect only to the selected room.
+     * Can only connect to one room at a time.
      * 
      * @param MessageManager manager that handle message and connect/disconnect
      */
@@ -85,10 +96,13 @@ public class TestMessenger {
 
             switch (input) {
                 case "connect":
+                    System.out.println("Connect to room");
+                    currRoom = keyboard.nextInt();
+                    keyboard.nextLine();
                     
-                    manager.connect(listener, listen_port);
+                    manager.connect(listener, listen_ports.get(currRoom));
                     
-                    System.out.println("Connected to server");
+                    System.out.println("Connected to room" + currRoom);
                     System.out.println("Please input user name");
                     userName = keyboard.nextLine();
                     break;
@@ -96,7 +110,7 @@ public class TestMessenger {
                     manager.disconnect(listener);
                     isConnected = false;
                 default:
-                    manager.sendMessage(userName, input, send_port, listen_port);
+                    manager.sendMessage(userName, input, send_ports.get(currRoom), listen_ports.get(currRoom));
             }
         }
 
