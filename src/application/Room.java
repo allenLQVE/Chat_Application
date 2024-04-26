60cd57d8f2b02005d81461fba47ded76743cbaba
allenLQVE <allenlqve@gmail.com>
2024-04-26 15:09:29 -0400

    Use Room and User object in the messenger

--- src/application/Room.java
+++ src/application/Room.java
@@ -5,17 +5,19 @@ import java.util.ArrayList;
 public class Room {
 	
 	private String name;
+	private int port;
 	private ArrayList<User> users;
-	private ArrayList<Message> contents;
+	private ArrayList<String> contents;
 	
-	Room(){
-		this("Room",new ArrayList<User>(), new ArrayList<Message>());
+	Room(String name, int port){
+		this(name, new ArrayList<User>(), new ArrayList<String>(), port);
 	}
 	
-	Room(String name, ArrayList<User> users, ArrayList<Message> contents){
+	Room(String name, ArrayList<User> users, ArrayList<String> contents, int port){
 		this.name = name;
 		this.users = users;
 		this.contents = contents;
+		this.port = port;
 	}
 
 	public ArrayList<User> getUsers() {
@@ -31,17 +33,14 @@ public class Room {
 		this.users.remove(user);
 	}
 	
-	public ArrayList<Message> getContents() {
+	public ArrayList<String> getContents() {
 		return contents;
 	}
-	public void setContents(ArrayList<Message> contents) {
+	public void setContents(ArrayList<String> contents) {
 		this.contents = contents;
 	}
-	public void addContent(Message content) {
+	public void addContent(String content) {
 		this.contents.add(content);
-	}
-	public void removeContent(Message content) {
-		this.contents.remove(content);
 	}
 
 	public String getName() {
@@ -51,6 +50,13 @@ public class Room {
 		this.name = name;
 	}
 
+	public void setPort(int port) {
+		this.port = port;
+	}
+	public int getPort() {
+		return port;
+	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if(obj.getClass() != this.getClass()){
@@ -58,7 +64,7 @@ public class Room {
 		}
 
 		Room room = (Room) obj;
-		return room.getName() == name && room.getUsers().equals(users);
+		return room.getPort() == port;
 	}
 	
 	@Override
